5c7975afabfcaeda9c15a338b05f035a1d260cbe
alexandrepeclat <web.alexandre.peclat@gmail.com>
2025-01-08 21:36:25 +0100

    code refactoré, plus clair, un seul compteur pour les panels

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-#include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <Servo.h>
+#include <ESP8266WiFi.h>
 #include <Encoder.h>
+#include <Servo.h>
 
-//TODO MECANIQUE : 
+// TODO MECANIQUE :
 //- Faciliter l'ajustement de la roue optique
-//  - en roue libre ?
-//  - fixation par le haut ?
-//  - fentes pour voir le trou ?
-//  - ligne visuelle pour guider sur la roue ?
+//   - en roue libre ?
+//   - fixation par le haut ?
+//   - fentes pour voir le trou ?
+//   - ligne visuelle pour guider sur la roue ?
 //- Aligner fente avec dent car ça coince mieux sur cette position
 //- Revoir alignement dents et trou de l'encodeur ? car 1 dent = 1 panneau doit tomber pile dans une step pleine de l'encodeur
 //- Permettre de découpler les roues ? dur
@@ -18,190 +18,200 @@
 //- Voir comment fixer roue encodeur + rondelles
 //- Prévoir plus de place pour pins capteur optique
 
-//TODO SOFTWARE :
+// TODO SOFTWARE :
 //- Sécurités si valeur hors norme, stoppe servo
+//- Ajouter un état "OFF" pour le servo qu'on peut toggler pour la sécurité
+//- Remplacer service setCurrentPanel par un service qui met l'état indéfini sur le panel courant (+ gérer panel courant indéfini tourne un tour jusqu'à déclencher l'optique)
+//- Du coup démarrer en "indéfini" mais ne pas bouger, et à la première commande, aller jusqu'à l'optique au minimum
+
+//#define DEBUG_ENABLED 1
 
 // Pins configuration
 #define SERVO_PIN D3
-#define ENCODER_PIN_A D1      // CLK
-#define ENCODER_PIN_B D2      // DT
-#define OPTICAL_SENSOR_PIN D6 // Pin pour le capteur optique
+#define ENCODER_PIN_A D1       // CLK
+#define ENCODER_PIN_B D2       // DT
+#define OPTICAL_SENSOR_PIN D6  // Pin pour le capteur optique
 
 // Constants
-const int PANELS_COUNT = 62;    // Nombre total de panneaux
-const int PULSES_PER_PANEL = 2; // Ajuste pour 4 impulsions par panneau
-const bool SENSOR_STATE = LOW;  // Constante qui définit l'état du capteur optique (LOW ou HIGH) lorsqu'il est au panneau 0
-const int DEFAULT_PANEL = 3;    // Panneau situé en position 0 selon capteur optique
+const int PANELS_COUNT = 62;              // Nombre total de panneaux
+const int PULSES_PER_PANEL = 2;           // Ajuste pour 4 impulsions par panneau
+const bool OPTICAL_DETECTED_STATE = LOW;  // Constante qui définit l'état du capteur optique (LOW ou HIGH) lorsqu'il est au panneau 0
+const int DEFAULT_PANEL = 3;              // Panneau situé en position 0 selon capteur optique
+const int ENCODER_DIRECTION_SIGN = -1;
+
 // Servo configuration
 Servo servo;
-const int STOP_SPEED = 90; // Valeur pour arrêter le servo
-const int RUN_SPEED = 120;  // Vitesse du servo pour avancer (91-180) 140 c'est bien 
+const int STOP_SPEED = 90;  // Valeur pour arrêter le servo
+const int RUN_SPEED = 120;  // Vitesse du servo pour avancer (91-180) 140 c'est bien
+static_assert(RUN_SPEED > STOP_SPEED, "RUN_SPEED must be greater than STOP_SPEED");
 
 // Globals
 ESP8266WebServer server(80);
 Encoder encoder(ENCODER_PIN_A, ENCODER_PIN_B);
-int currentPanel = 0; // Panel actuel
-int targetPanel = 0;  // Panneau cible
+int targetPanel = 0;   // Panneau cible
 
 // Setup Wi-Fi
-const char *ssid = "ap8F2EOjLm";
-const char *password = "gsecumonwifiii123";
+const char* ssid = "ap8F2EOjLm";
+const char* password = "gsecumonwifiii123";
+
+int getCurrentPulses() {
+  int pulses = encoder.read() * ENCODER_DIRECTION_SIGN;
+  pulses = pulses % (PANELS_COUNT * PULSES_PER_PANEL);
+  return pulses;
+}
+
+int getCurrentPanel() {
+  return getCurrentPulses() / PULSES_PER_PANEL;
+}
+
+void setCurrentPulses(int pulses) {
+  pulses = pulses % (PANELS_COUNT * PULSES_PER_PANEL);
+  encoder.write(pulses * ENCODER_DIRECTION_SIGN);
+}
+
+void setCurrentPanel(int panel) {
+  setCurrentPulses(panel * PULSES_PER_PANEL);
+}
+
+int getTargetPulses() {
+  return targetPanel * PULSES_PER_PANEL;
+}
+
+String buildDebugString() {
+  String debugString = "currentPanel: " + String(getCurrentPanel()) +
+                       " targetPanel:" + String(targetPanel) +
+                       " currentPulses:" + String(getCurrentPulses()) +
+                       " targetPulses:" + String(getTargetPulses()) +
+                       " rawPulses:" + String(encoder.read()) +
+                       " optical:" + String(digitalRead(OPTICAL_SENSOR_PIN));
+  return debugString;
+}
+
+String buildDebugJson(String message) {
+  String debugJson = "{";
+  debugJson += "\"message\":\"" + message + "\",";
+  debugJson += "\"currentPanel\":" + String(getCurrentPanel()) + ",";
+  debugJson += "\"targetPanel\":" + String(targetPanel) + ",";
+  debugJson += "\"currentPulses\":" + String(getCurrentPulses()) + ",";
+  debugJson += "\"targetPulses\":" + String(getTargetPulses()) + ",";
+  debugJson += "\"rawPulses\":" + String(encoder.read()) + ",";
+  debugJson += "\"optical\":" + String(digitalRead(OPTICAL_SENSOR_PIN));
+  debugJson += "}";
+  return debugJson;
+}
+
+//#ifdef DEBUG_ENABLED
+unsigned long lastDebugPrintTime = 0;
+void serialPrintThrottled(String message) {
+  unsigned long currentTime = millis();
+  if (currentTime - lastDebugPrintTime > 100) {
+    Serial.println(message);
+    lastDebugPrintTime = currentTime;
+  }
+}
+//#endif
 
 // Functions
 
-  unsigned long lastPrintTime = 0;
-void serialPrintThrottled(String message)
-{
-  unsigned long currentTime = millis();
-  if (currentTime - lastPrintTime > 100)
-  {
-    Serial.println(message);
-    lastPrintTime = currentTime;
-  }
-}
-
-void sendHeaders()
-{
+void sendHeaders() {
   server.sendHeader("Access-Control-Allow-Origin", "*");
 }
 
-void handleGetDebug()
-{
+void handleGetDebug() {
   sendHeaders();
-  int currentPulses = abs(encoder.read());                                 // Compte total des impulsions
-  currentPulses = currentPulses % (PANELS_COUNT * PULSES_PER_PANEL); // Convertit les impulsions en panneaux
-  int targetPulses = targetPanel * PULSES_PER_PANEL;                  // Pulses correspondant au panneau cible
-  currentPanel = currentPulses / PULSES_PER_PANEL;
-  server.send(200, "text/plain", "Current panel: " + String(currentPanel) + ", Target panel: " + String(targetPanel) + ", Current pulses: " 
-  + String(currentPulses) + ", Target pulses: " + String(targetPulses) + ", Optical: " + String(digitalRead(OPTICAL_SENSOR_PIN)));
+  server.send(200, "text/plain", buildDebugJson("Debug request"));
 }
 
-void handleGetCurrentPanel()
-{
+void handleGetCurrentPanel() {
   sendHeaders();
-  server.send(200, "text/plain", String(currentPanel));
+  server.send(200, "text/plain", String(getCurrentPanel()));
 }
 
-void handleMoveToPanel()
-{
+void handleMoveToPanel() {
   sendHeaders();
-  if (server.hasArg("panel"))
-  {
+  if (server.hasArg("panel")) {
     targetPanel = server.arg("panel").toInt();
-    targetPanel %= PANELS_COUNT; // Assure que la cible est dans les limites
-    server.send(200, "text/plain", "Moving to panel " + String(targetPanel));
-  }
-  else
-  {
+    targetPanel %= PANELS_COUNT;  // Assure que la cible est dans les limites
+    server.send(200, "text/plain", buildDebugJson("Moving to panel " + String(targetPanel)));
+  } else {
     server.send(400, "text/plain", "Missing 'panel' argument");
   }
 }
 
-void handleAdvancePanels()
-{
+void handleAdvancePanels() {
   sendHeaders();
-  if (server.hasArg("count"))
-  {
+  if (server.hasArg("count")) {
     int advanceCount = server.arg("count").toInt();
+    int currentPanel = getCurrentPanel();
     targetPanel = (currentPanel + advanceCount) % PANELS_COUNT;
-    server.send(200, "text/plain", "From " + String(currentPanel) + ", Advancing " + String(advanceCount) + " panels to " + String(targetPanel));
-  }
-  else
-  {
+    server.send(200, "text/plain", buildDebugJson("From " + String(currentPanel) + ", Advancing " + String(advanceCount) + " panels to " + String(targetPanel)));
+  } else {
     server.send(400, "text/plain", "Missing 'count' argument");
   }
 }
 
-void handleSetCurrentPanel()
-{
+void handleSetCurrentPanel() {
   sendHeaders();
-  if (server.hasArg("panel"))
-  {
-    currentPanel = server.arg("panel").toInt();
-    currentPanel %= PANELS_COUNT; // Assure que la valeur est dans les limites
+  if (server.hasArg("panel")) {
+    int value = server.arg("panel").toInt();
+    setCurrentPanel(value); //Will set encoder accordingly and keep value within limits
+    int currentPanel = getCurrentPanel();
     targetPanel = currentPanel;
-    encoder.write(abs(currentPanel) * PULSES_PER_PANEL); //TODO Centraliser gestion direction de l'encodeur (les moins partout pas cool)
-    server.send(200, "text/plain", "Current panel set to " + String(currentPanel));
-  }
-  else
-  {
+    server.send(200, "text/plain", buildDebugJson("Current panel set to " + String(currentPanel)));
+  } else {
     server.send(400, "text/plain", "Missing 'panel' argument");
   }
 }
 
-void handleStop()
-{
+void handleStop() {
   sendHeaders();
   servo.write(STOP_SPEED);
+  int currentPanel = getCurrentPanel();
   targetPanel = currentPanel;
-  server.send(200, "text/plain", "Stopped. Current panel set to " + String(currentPanel));
+  server.send(200, "text/plain", buildDebugJson("Stopped. Current panel set to " + String(currentPanel)));
+}
+
+
+
+//TODO setTargetPanel ?
+
+// Non-blocking move logic
+void updateServoMovement() {
+  int currentPulses = getCurrentPulses();
+  int targetPulses = getTargetPulses();
+
+  // Vérifie si la position cible est atteinte
+  if (currentPulses == targetPulses) {
+    servo.write(STOP_SPEED);  // Arrête le servo si la cible est atteinte
+  } else {
+    servo.write(RUN_SPEED);  // Continue à faire tourner le servo
+  }
 }
 
 // Fonction qui vérifie l'état du capteur optique
-void checkOpticalSensor()
-{
+void checkOpticalSensor() {
   int sensorState = digitalRead(OPTICAL_SENSOR_PIN);
-  //Serial.println("Optical state: " + String(sensorState) + " currentPanel: " + String(currentPanel));
-  digitalWrite(LED_BUILTIN, sensorState);
-
-  if (sensorState == 0)
-  {
-    currentPanel = DEFAULT_PANEL; // Réinitialiser le panneau actuel à 0 lorsque le capteur détecte le panneau 0 //TODO à voir mais c'est pas nécessaire de setter ce compteur si on fait toujours le calcul dans la boucle du servo
-    encoder.write(DEFAULT_PANEL * PULSES_PER_PANEL); // Réinitialise l'encodeur à 0
-
-
-  //TODO VUE QUE MON ENCODEUR TOURNE A L'ENVERS, ça joue pas quand on sette le compteur à 3 positif 
-
+  if (sensorState == OPTICAL_DETECTED_STATE) {
+    setCurrentPanel(DEFAULT_PANEL);                     // Réinitialiser le panneau actuel à 0 lorsque le capteur détecte le panneau 0 //TODO à voir mais c'est pas nécessaire de setter ce compteur si on fait toujours le calcul dans la boucle du servo
+    
+        // TODO VU QUE MON ENCODEUR TOURNE A L'ENVERS, ça joue pas quand on sette le compteur à 3 positif
 
     serialPrintThrottled("Panneau 0 détecté, currentPanel réinitialisé à 0");
   }
 }
 
-
-
-// Non-blocking move logic
-void updateServoMovement()
-{
-   /*if (currentPanel == targetPanel)
-   {
-     return;
-   }*/
-
-  int currentPulses = abs(encoder.read());                                 // Compte total des impulsions
-  currentPulses = currentPulses % (PANELS_COUNT * PULSES_PER_PANEL); // Convertit les impulsions en panneaux
-  int targetPulses = targetPanel * PULSES_PER_PANEL;                  // Pulses correspondant au panneau cible
-  currentPanel = currentPulses / PULSES_PER_PANEL; //TODO séparer boucle servo et encodeur ? 
-
-  serialPrintThrottled("Current panel: " + String(currentPanel) + ", Target panel: " + String(targetPanel) + ", Current pulses: " 
-  + String(currentPulses) + ", Target pulses: " + String(targetPulses) + ", Optical: " + String(digitalRead(OPTICAL_SENSOR_PIN)));
-  // Vérifie si la position cible est atteinte
-  if (currentPulses == targetPulses)
-  {
-    servo.write(STOP_SPEED); // Arrête le servo si la cible est atteinte
-  }
-  else
-  {
-    servo.write(RUN_SPEED); // Continue à faire tourner le servo
-  }
-}
-
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   // Wi-Fi setup
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
   Serial.print("Connected to WiFi! ");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
-
-  // Pin setup
-  pinMode(OPTICAL_SENSOR_PIN, INPUT); // Configure le capteur optique en entrée
 
   // REST API routes
   server.on("/getCurrentPanel", HTTP_GET, handleGetCurrentPanel);
@@ -215,12 +225,18 @@ void setup()
 
   // Servo setup
   servo.attach(SERVO_PIN);
-  servo.write(STOP_SPEED); // Ensure the servo starts stopped
+  servo.write(STOP_SPEED);  // Ensure the servo starts stopped
+
+  // Optical sensor setup
+  pinMode(OPTICAL_SENSOR_PIN, INPUT);  // Configure le capteur optique en entrée
 }
 
-void loop()
-{
-  server.handleClient(); // Handle incoming HTTP requests
-  checkOpticalSensor();  // Vérifie l'état du capteur optique
-  updateServoMovement(); // Manage the servo's movement
+void loop() {
+  server.handleClient();  // Handle incoming HTTP requests
+  checkOpticalSensor();   // Vérifie l'état du capteur optique
+  updateServoMovement();  // Manage the servo's movement
+//#ifdef DEBUG_ENABLED
+  serialPrintThrottled(buildDebugString());
+//#endif
 }
+
