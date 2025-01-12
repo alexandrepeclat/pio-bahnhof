14a70e29dce37306e1a8f08b0d68d14cb585d508
alexandrepeclat <web.alexandre.peclat@gmail.com>
2025-01-12 15:14:21 +0100

    error stop

--- src/main.cpp
+++ src/main.cpp
@@ -30,14 +30,14 @@ void setMotorSpeed(float normalizedSpeed);
 //- Voir comment fixer axe roue optique + rondelles
 //- Voir comment fixer roue encodeur + rondelles
 //- Prévoir plus de place pour pins capteur optique
-//- Roue libre à insérer une fois que tout est en place pour permettre de libérer le mécanisme en manuel ? 
+//- Roue libre à insérer une fois que tout est en place pour permettre de libérer le mécanisme en manuel ?
 
 // TODO SOFTWARE :
 //- Sécurités si valeur hors norme, stoppe servo
 //- Remplacer service setCurrentPanel par un service calibration qui met l'état indéfini sur le panel courant (+ gérer panel courant indéfini tourne un tour jusqu'à déclencher l'optique)
 //- Du coup démarrer en "indéfini" mais ne pas bouger, et à la première commande, aller jusqu'à l'optique au minimum
 // TODO setTargetPanel ?
-// TODO !!!!!!!!!!!! FAIRE UN MOVEUNTILOPTICALEDGE service pour avancer jusqu'à l'optique ? afficher le currentPulses et cie quand optical est détecté dans debug ? 
+// TODO !!!!!!!!!!!! FAIRE UN MOVEUNTILOPTICALEDGE service pour avancer jusqu'à l'optique ? afficher le currentPulses et cie quand optical est détecté dans debug ?
 // TODO ? stocker la valeur de l'encodeur dans variable et la remettre à zéro avec l'optique, mais laisser la valeur de l'encodeur originale. Permettrait de voir s'il y a un décalage au début et s'il y a un décalage progressif du à ratés en raison d'une boucle trop lente
 // - ou plutôt afficher la valeur de l'encodeur brute lors du passage de l'optique
 // - et faire une variable previousPulses pour voir si une boucle ne loupe pas un step
@@ -51,16 +51,15 @@ void setMotorSpeed(float normalizedSpeed);
 #define OPTICAL_SENSOR_PIN D6  // Pin pour le capteur optique
 
 // Constants
-const int PANELS_COUNT = 62;              // Nombre total de panneaux
-const int PULSES_PER_PANEL = 2;           // Ajuste pour 4 impulsions par panneau
-const int DEFAULT_PANEL = 40;              // Panel at optical sensor position (si on calibre, il s'arrête sur le panel précédent l'optique, donc s'il s'arrête au 3, l'optique est au 4)
+const int PANELS_COUNT = 62;     // Nombre total de panneaux
+const int PULSES_PER_PANEL = 2;  // Ajuste pour 4 impulsions par panneau
+const int DEFAULT_PANEL = 40;    // Panel at optical sensor position (si on calibre, il s'arrête sur le panel précédent l'optique, donc s'il s'arrête au 3, l'optique est au 4)
 const int ENCODER_DIRECTION_SIGN = -1;
 const int OFFSET = 0;  // Offset between pulses and panel, must not exceed PULSES_PER_PANEL
 static_assert(OFFSET < PULSES_PER_PANEL, "OFFSET must be less than PULSES_PER_PANEL");
 static_assert(OFFSET >= 0, "OFFSET must be non-negative");
 
-#define OPTICAL_EDGE RISING// Define the edge type for setting the current panel
-
+#define OPTICAL_EDGE RISING  // Define the edge type for setting the current panel
 
 // Servo configuration
 Servo servo;
@@ -71,10 +70,12 @@ static_assert(RUN_SPEED > 90, "RUN_SPEED must be greater than 90 or everything w
 // Globals
 ESP8266WebServer server(80);
 Encoder encoder(ENCODER_PIN_A, ENCODER_PIN_B);
-int targetPanel = 0;        // Panneau cible //TODO utiliser un targetPulses et virer quasi tous les appels get/setPanel sauf depuis l'api ou les debug
-bool motorEnabled = false;  // Boolean to enable/disable motor, starts disabled
-int encoderValue = 0;       // Variable to store the encoder value
-int lastSensorState = HIGH; // Initialize to HIGH (not detected)
+int targetPanel = 0;         // Panneau cible //TODO utiliser un targetPulses et virer quasi tous les appels get/setPanel sauf depuis l'api ou les debug
+bool motorEnabled = false;   // Boolean to enable/disable motor, starts disabled
+int encoderValue = 0;        // Variable to store the encoder value
+int lastSensorState = HIGH;  // Initialize to HIGH (not detected)
+bool errorFlag = false;      // Emergency stop flag
+String errorMessage = "";    // Emergency stop message
 
 #ifdef DEBUG_ENABLED
 std::map<String, String> lastDebugMessages;  // Map to store the last debug messages
@@ -110,13 +111,15 @@ int getTargetPulses() {
 
 String buildDebugJson(String message) {
   String debugJson = "{";
-  debugJson += "\"message\":\"" + message + "\",";
-  debugJson += "\"currentPanel\":" + String(getCurrentPanel()) + ",";
-  debugJson += "\"targetPanel\":" + String(targetPanel) + ",";
-  debugJson += "\"currentPulses\":" + String(getCurrentPulses()) + ",";
-  debugJson += "\"targetPulses\":" + String(getTargetPulses()) + ",";
-  debugJson += "\"rawPulses\":" + String(encoderValue) + ",";
-  debugJson += "\"optical\":" + String(digitalRead(OPTICAL_SENSOR_PIN));
+  debugJson += "\"message\":\"" + message + "\"," +
+               "\"currentPanel\":" + String(getCurrentPanel()) + "," +
+               "\"targetPanel\":" + String(targetPanel) + "," +
+               "\"currentPulses\":" + String(getCurrentPulses()) + "," +
+               "\"targetPulses\":" + String(getTargetPulses()) + "," +
+               "\"rawPulses\":" + String(encoderValue) + "," +
+               "\"optical\":" + String(digitalRead(OPTICAL_SENSOR_PIN)) + "," +
+               "\"errorFlag\":" + String(errorFlag) + "," +
+               "\"errorMessage\":\"" + errorMessage + "\"";
   debugJson += "}";
   return debugJson;
 }
@@ -130,7 +133,9 @@ String buildDebugString() {
                        " rawPulses:" + String(encoderValue) +
                        " optical:" + String(digitalRead(OPTICAL_SENSOR_PIN)) +
                        " distance: " + String(getRemainingPulses()) +
-                       " speed: " + String(calculateEaseOutSpeed());
+                       " speed: " + String(calculateEaseOutSpeed()) +
+                       " errorFlag: " + String(errorFlag) +
+                       " errorMessage: " + errorMessage;
 
   return debugString;
 }
@@ -184,11 +189,11 @@ void handleAdvancePanels() {
   }
 }
 
-void handleCalibrate() { //TODO a voir pour gérer le process de calibration autrement ? via flag ? j'aimerais le faire revenir sur le même panel après calibration
+void handleCalibrate() {  // TODO a voir pour gérer le process de calibration autrement ? via flag ? j'aimerais le faire revenir sur le même panel après calibration
   sendHeaders();
-  setCurrentPanel(DEFAULT_PANEL);  // Set current panel to default
+  setCurrentPanel(DEFAULT_PANEL);                                   // Set current panel to default
   targetPanel = (DEFAULT_PANEL - 1 + PANELS_COUNT) % PANELS_COUNT;  // Set target panel to the one before
-  motorEnabled = true;  // Enable motor for calibration
+  motorEnabled = true;                                              // Enable motor for calibration
   server.send(200, "text/plain", buildDebugJson("Calibration started. Current panel set to " + String(DEFAULT_PANEL) + ", target panel set to " + String(targetPanel)));
 }
 
@@ -205,7 +210,7 @@ int getRemainingPulses() {
   int currentPulses = getCurrentPulses();
   int targetPulses = getTargetPulses();
   int totalPulses = PANELS_COUNT * PULSES_PER_PANEL;
-  int distance = (targetPulses - currentPulses + totalPulses) % totalPulses;  // Calcule la distance en avance (horaire) //TODO on doit tjrs avoir du positif ici asserter ça
+  int distance = (targetPulses - currentPulses + totalPulses) % totalPulses;  // Calcule la distance en avance (horaire)
   return distance;
 }
 
@@ -217,8 +222,8 @@ float calculateEaseOutSpeed() {
   int remainingPulses = getRemainingPulses();
 
   if (remainingPulses == 0) {  // TODO parfois j'ai un décalage de 1... peut être même problème que dans l'ancien code commenté...
-    motorEnabled = false;  // Disable motor when target is reached
-    return 0.f;                              // Normalized speed for stop
+    motorEnabled = false;      // Disable motor when target is reached //TODO à voir si motorEnabled dirige le setMotorSpeed ou si c'est l'inverse et à voir pour refactorer où est-ce qu'on le lit/écrit
+    return 0.f;                // Normalized speed for stop
   } else if (remainingPulses <= PULSES_PER_PANEL) {
     return 0.3f;
   } else if (remainingPulses <= PULSES_PER_PANEL * 3) {
@@ -261,7 +266,6 @@ void checkOpticalSensor() {
   //- ça détecte pas au step 1 suivant (qui serait le 2) et on a un décalage
   //- à voir aussi si nécessite pas un ajustement mécanique
   //- ou alors ne détecter l'optique qu'aux steps pairs ou impairs (selon offset) et virer l'offset ailleurs
-
 }
 
 void setup() {
@@ -278,7 +282,7 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   // REST API routes
-  server.on("/getCurrentPanel", HTTP_GET, handleGetCurrentPanel); //TODO renommer en virant les gets et les mots panel des actions
+  server.on("/getCurrentPanel", HTTP_GET, handleGetCurrentPanel);  // TODO renommer en virant les gets et les mots panel des actions
   server.on("/getDebug", HTTP_GET, handleGetDebug);
   server.on("/moveToPanel", HTTP_POST, handleMoveToPanel);
   server.on("/advancePanels", HTTP_POST, handleAdvancePanels);
@@ -292,14 +296,14 @@ void setup() {
   setMotorSpeed(0);  // Ensure the servo starts stopped using the centralized function
 
   // Optical sensor setup
-  pinMode(OPTICAL_SENSOR_PIN, INPUT);  // Configure le capteur optique en entrée
+  pinMode(OPTICAL_SENSOR_PIN, INPUT);                 // Configure le capteur optique en entrée
   lastSensorState = digitalRead(OPTICAL_SENSOR_PIN);  // Initialize lastSensorState
 }
 
 void loop() {
   encoderValue = encoder.read();  // Update encoder value at the beginning of each loop
-  checkOpticalSensor();   // Vérifie l'état du capteur optique
-  updateServoMovement();  // Manage the servo's movement
+  checkOpticalSensor();           // Vérifie l'état du capteur optique
+  updateServoMovement();          // Manage the servo's movement
 #ifdef DEBUG_ENABLED
   serialPrintThrottled("ALL", buildDebugString());
 #endif
@@ -307,7 +311,20 @@ void loop() {
 }
 
 void setMotorSpeed(float normalizedSpeed) {
+  if (!motorEnabled || errorFlag)
+    servo.write(STOP_SPEED);
   normalizedSpeed = constrain(normalizedSpeed, 0.f, 1.f);
   int speed = map(normalizedSpeed * 100, 0, 100, STOP_SPEED, RUN_SPEED);
+  if (speed < 90) {
+    emergencyStop("Backwards speed is not allowed");
+    return;
+  }
   servo.write(speed);
+}
+
+void emergencyStop(String message) {
+  errorFlag = true;
+  errorMessage = message;
+  servo.write(STOP_SPEED);  // Stop the motor immediately
+  Serial.println("EMERGENCY STOP: " + message);
 }
