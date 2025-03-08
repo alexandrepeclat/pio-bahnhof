c3ec7fd2de28e8c80af35df7c63cc33f0b973f1b
alexandrepeclat <web.alexandre.peclat@gmail.com>
2025-03-08 19:52:36 +0100

    comments

--- src/JsonSettingsBase.h
+++ src/JsonSettingsBase.h
@@ -1,52 +1,79 @@
 #include <ArduinoJson.h>
 #include <LittleFS.h>
 
+/**
+ * Base class for settings that can saved and loaded from the file system into a JSON file.
+ * Must be inherited by a class that will define the properties to save and load.
+ */
 class JsonSettingsBase {
-protected:
-    String filename;
+ protected:
+  String filename;
 
-public:
-    JsonSettingsBase(const String& file) : filename(file) {}
+ public:
+  /**
+   * Base constructor, must be called from the derived class for the settings file to be set.
+   * @param file The file name to use for the settings.
+   */
+  JsonSettingsBase(const String& file)
+      : filename(file) {}
 
-    virtual void serialize(JsonDocument& doc) = 0;
-    virtual void deserialize(JsonDocument& doc) = 0;
+      /**
+       * Derived class must implement this method to serialize its properties into the JSON document.
+       * @param doc The JSON document to serialize into. //TODO vérifier que c'est correct les commentaires (pour les deux méthodes)
+       */
+  virtual void serialize(JsonDocument& doc) = 0;
 
-    bool load() {
-        File file = LittleFS.open(filename, "r");
-        if (!file) {
-            return false;
-        }
+    /**
+     * Derived class must implement this method to deserialize its properties from the JSON document.
+     * @param doc The JSON document to deserialize from.
+     */
+  virtual void deserialize(JsonDocument& doc) = 0;
 
-        JsonDocument doc;
-        DeserializationError error = deserializeJson(doc, file);
-        file.close();
-        if (error) {
-            return false;
-        }
-
-        deserialize(doc);
-        return true;
+  /**
+   * Load the settings from the file system.
+   */
+  bool load() {
+    File file = LittleFS.open(filename, "r");
+    if (!file) {
+      return false;
     }
 
-    bool save() {
-        JsonDocument doc;
-        serialize(doc);
-
-        File file = LittleFS.open(filename, "w");
-        if (!file) {
-            return false;
-        }
-
-        serializeJson(doc, file);
-        file.close();
-        return true;
+    JsonDocument doc;
+    DeserializationError error = deserializeJson(doc, file);
+    file.close();
+    if (error) {
+      return false;
     }
 
-    String getContent() {
-        JsonDocument doc;
-        serialize(doc); // Sérialise les propriétés actuelles dans le doc
-        String jsonString;
-        serializeJson(doc, jsonString); // Convertit le doc en string
-        return jsonString;
+    deserialize(doc);
+    return true;
+  }
+
+  /**
+   * Save the settings to the file system.
+   */
+  bool save() {
+    JsonDocument doc;
+    serialize(doc);
+
+    File file = LittleFS.open(filename, "w");
+    if (!file) {
+      return false;
     }
+
+    serializeJson(doc, file);
+    file.close();
+    return true;
+  }
+
+  /**
+   * Returns JSON (readable) content of the settings.
+   */
+  String getContent() {
+    JsonDocument doc;
+    serialize(doc);  // Sérialise les propriétés actuelles dans le doc
+    String jsonString;
+    serializeJson(doc, jsonString);  // Convertit le doc en string
+    return jsonString;
+  }
 };
